f3a5f9dbca27e7600a599fa2de5029b635ee02e4
salman <salmanvit12345@gmail.com>
2018-02-13 08:43:08 +0530

    modified

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const path=require('path')
 const app=express()
+const SERVER_PORT=process.env.PORT||2020
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use('/api',require('./api'))
-app.listen(2020,()=>{
+app.listen(SERVER_PORT,()=>{
     console.log("started the base")
 })
